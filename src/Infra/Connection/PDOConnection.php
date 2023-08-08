e071e1428fba680bec57328102c746aa2006aa13
Hamza Benbrahim <benbrahim.hamza@proton.me>
2023-08-08 17:04:20 +0200

    feat: updated pdo connection and env file

--- src/Infra/Connection/PDOConnection.php
+++ src/Infra/Connection/PDOConnection.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace Fulll\Infrastructure\Connection;
+namespace Fulll\Infra\Connection;
 
 use Exception;
 use PDO;
+use PDOException;
 
 class PDOConnection
 {
@@ -18,9 +19,9 @@ class PDOConnection
     public function __construct()
     {
         try {
-            $this->pdoConnection = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] . ';charset=utf8', $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
-        } catch (Exception $e) {
-            throw($e);
+            $this->pdoConnection = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] . ';charset=' . $_ENV['DB_CHARSET'] . ';port=' . $_ENV['DB_PORT'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+        } catch (PDOException $e) {
+            throw ($e);
         }
     }
 
