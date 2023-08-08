fcee8968f4b0ceecfb7bd9461cb5bfe93ac373c1
Hamza Benbrahim <benbrahim.hamza@proton.me>
2023-08-08 17:02:53 +0200

    feat: added command line init with dotenv

---
+++ bin/fleet.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Fulll\App\Command\CreateFleetCommand;
+use Fulll\App\Command\LocalizeVehicleCommand;
+use Fulll\App\Command\RegisterVehicleCommand;
+use Fulll\Domain\Repository\FleetRepository;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/../.env');
+
+$application = new Application();
+
+$application->add(new CreateFleetCommand(new FleetRepository()));
+$application->add(new LocalizeVehicleCommand());
+$application->add(new RegisterVehicleCommand());
+
+$application->run();
