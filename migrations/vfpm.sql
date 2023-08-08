e0c68cfeada7eb64bf3442172abe6689d5283bf0
Hamza Benbrahim <benbrahim.hamza@proton.me>
2023-08-08 17:03:37 +0200

    feat: updated sql dump for auto increment

--- migrations/vfpm.sql
+++ migrations/vfpm.sql
@@ -1,3 +1,4 @@
+
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
@@ -9,12 +10,11 @@ SET NAMES utf8mb4;
 
 DROP TABLE IF EXISTS `Fleet`;
 CREATE TABLE `Fleet` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
-  KEY `userId` (`userId`),
-  CONSTRAINT `Fleet_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `User` (`id`)
+  KEY `userId` (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
@@ -24,22 +24,14 @@ CREATE TABLE `FleetVehicle` (
   `idVehicle` int(11) NOT NULL,
   KEY `idFleet` (`idFleet`),
   KEY `idVehicle` (`idVehicle`),
-  CONSTRAINT `FleetVehicle_ibfk_1` FOREIGN KEY (`idFleet`) REFERENCES `Fleet` (`id`) ON DELETE NO ACTION,
-  CONSTRAINT `FleetVehicle_ibfk_2` FOREIGN KEY (`idVehicle`) REFERENCES `Vehicle` (`id`) ON DELETE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
-DROP TABLE IF EXISTS `User`;
-CREATE TABLE `User` (
-  `id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
+  CONSTRAINT `FleetVehicle_ibfk_1` FOREIGN KEY (`idFleet`) REFERENCES `Fleet` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FleetVehicle_ibfk_2` FOREIGN KEY (`idVehicle`) REFERENCES `Vehicle` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 DROP TABLE IF EXISTS `Vehicle`;
 CREATE TABLE `Vehicle` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `plateNumber` varchar(36) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
